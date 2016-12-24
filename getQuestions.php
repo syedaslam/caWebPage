0eaab0ff19c95c9eaca928c5982db03ccfeaaa26
syedaslam <ammuaslam689@gmail.com>
2016-12-24 01:13:37 -0800

    Update getQuestions.php

--- getQuestions.php
+++ getQuestions.php
@@ -8,25 +8,22 @@ if($conn ->connect_error){
 	die("Connection Failed: ".$conn->connect_error);
 	}
 //echo "Connected successfully";
-
 $queData = array();
-
 $qId = $_POST['qId'];
-
 $subTagId = 9; //$_POST['subtagname'];
-
 if($conn->connect_error){
     echo 'Connection Faild: '.$con->connect_error;
     }else{
-        $sql='SELECT * FROM mcquestions_hi where question_id='.$qId;
+        $sql='SELECT * FROM mcquestions_hi where question_id=6'; //.$qId;
 		$res=$conn->query($sql);
          while ($row = $res->fetch_assoc()) {
-
-         echo $row['question'];
-		 
-
+         $queData["que"]= $row['question'];
+		 $queData["opt1"]=$row["option1"];
+		 $queData["opt2"]=$row["option2"];
+		 $queData["opt3"]=$row["option3"];
+		 $queData["opt4"]=$row["option4"];
+		 $queData["opt5"]=$row["option5"];
  }
-
-// echo $queData;
+echo json_encode($queData);
         }
 ?>
