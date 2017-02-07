0678ccae49b7bf5397090793d92ddca66a12f247
anilkunchalaece@gmail.com <anilkunchalaece@gmail.com>
2017-02-07 15:47:19 +0530

    added config file inserver side

---
+++ work/serverSide/config.php
@@ -0,0 +1,14 @@
+<?php
+$serverName = "localhost";
+$userName = "anil";
+$password = "anil";
+$db = "virtualm_jobportal";
+
+$conn = new mysqli($serverName,$userName,$password,$db);
+
+if($conn->connect_error){
+    echo 'Connection Faild: '.$con->connect_error;
+    }else{
+      echo 'Connected';
+    }
+?>
